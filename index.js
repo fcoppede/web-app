3e7ebd443ed558d5b2e2483976169c72977024cf
fcoppede <fcoppede@gmail.com>
2021-03-31 18:54:23 -0300

    Update index.js
    new update

--- index.js
+++ index.js
@@ -40,10 +40,16 @@ app.use(
 
 app.use(
  auth({
-   secret: SESSION_SECRET,
+   secret: CLIENT_SECRET,
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
+   // 👇 add this 👇
+   authorizationParams: {
+     response_type: "code id_token",
+     audience: "https://expenses-api",
+   },
+   // 👆 add this 👆
  })
 );
 
@@ -71,17 +77,25 @@ app.get("/user", requiresAuth(), async (req, res) => {
   });
 });
 
- app.get("/expenses", requiresAuth(), async (req, res, next) => {
-   try {
-     const expenses = await axios.get(`${API_URL}/reports`);
-     res.render("expenses", {
-       user: req.oidc && req.oidc.user,
-       expenses: expenses.data,
-     });
-   } catch (err) {
-     next(err);
-   }
- });
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
+ try {
+   // 👇 get the token from the request 👇
+   const { token_type, access_token } = req.oidc.accessToken;
+   // 👇 then send it as an authorization header 👇
+   const expenses = await axios.get(`${API_URL}/reports`, {
+     headers: {
+       Authorization: `${token_type} ${access_token}`,
+     },
+   });
+   // 👆 end of changes 👆
+   res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+ } catch (err) {
+   next(err);
+ }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
